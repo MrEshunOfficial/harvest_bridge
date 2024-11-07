ab6e50edb2f64bdd7a38bcc0bf799691f949305f
christopher <christophereshun91@gmail.com>
2024-11-07 20:16:03 +0000

    initialization and deployment phase

---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="w-full p-3 flex items-center justify-between">
+      Header
+    </header>
+  );
+}
